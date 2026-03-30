f9b0b6b675d66f1454c2b7c0fdfa7db90c34dcc6
p.siqueira <p.siqueira@cigamgestor.com.br>
2026-03-30 01:51:49 -0300

    "feat: adiciona cors food"

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ const app = express();
 app.use(cors({
   origin: [
     "https://trovare-beauty.vercel.app",
+    "https://trovare-food.vercel.app",
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
